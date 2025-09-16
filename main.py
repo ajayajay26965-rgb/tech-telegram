e4fd280946b6ae96874c523d15bae474aa186a66
ajayajay26965-rgb <ajayajay26965@gmail.com>
2025-09-16 08:11:56 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram import Client. filters
+
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+moviebot = Client(
+    name="Samplebot",
+    api_id=API_ID,
+    api_hash=API_HASH
+    bot_token=BOT_TOKEN
+)
+
+
+print("Bot was Started")
+
+moviebot.run()
